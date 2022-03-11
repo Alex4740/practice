ad0c101093bd36024ebe5f4b12b9fc8f4877e115
Alemayehu145 <100173700+Alemayehu145@users.noreply.github.com>
2022-03-11 15:52:37 -0500

    commit my work

---
+++ customer_class/customer_dao_interf.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+from Entities.customer import Customer
+
+
+class CustomerDAOInterface(ABC):
+
+    @abstractmethod
+    def insert_into_customer_table(self, customer_obj: Customer) -> Customer:
+        pass
+
+    @abstractmethod
+    def delete_from_customer_table_by_id(self, customer_id: int) -> bool:
+        pass
+
+    
